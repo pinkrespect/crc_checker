4681a0d3511e156a9911a7f79741278542eb2599
Maisie <miss.sonagi@gmail.com>
2017-05-29 19:26:13 +0900

    Edit CRC.java

--- CRC/src/CRC.java
+++ CRC/src/CRC.java
@@ -2,39 +2,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class CRC {
-    static int[] CRC_Checker(int[] input) {
+    static void CRC_Checker(int[] input) {
         int[] divisor = new int[]{1, 1, 0, 1, 0, 1};
-        int[] result = new int[input.length + divisor.length - 1];
-        ArrayList dividend = new ArrayList();
+        int[] dividend = IntStream.concat(Arrays.stream(input),
+                IntStream.generate(() -> 0).limit(divisor.length-1))
+                .toArray();
 
-        for(int i = 0; i < result.length; i++){
-            if(i < input.length)
-                dividend.add(input[i]);
-            else
-                dividend.add(0);
-        }
-
-        IntStream.range(0, result.length).forEachOrdered(i -> {
-            if((int)dividend.get(i) == 1){
+        IntStream.range(0, input.length).forEach(i -> dividend[i] = input[i]);
+        IntStream.range(0, input.length).forEachOrdered(i -> {
+            if(dividend[i] == 1){
                 IntStream.range(0, divisor.length).forEachOrdered(j -> {
-                    dividend.set(i+j, divisor[j] ^ (int)dividend.get(i+j));
+                    dividend[i+j] = divisor[j] ^ dividend[i+j];
                 });
             }
         });
 
-        for(int i = 0; i < dividend.size(); i++){
-            if(i < input.length)
-                result[i] = input[i];
-            else
-                result[i] = (int)dividend.get(i);
-        }
-
-        return result;
     }
 
     public static void main(String args[]) {
-        System.out.println(Arrays.toString(CRC_Checker(new int[]{1, 0, 1, 0, 0, 0, 1, 1, 0, 1})));
+        //System.out.println(Arrays.toString(
+                CRC_Checker(new int[]{1, 0, 1, 0, 0, 0, 1, 1, 0, 1});
     }
 }
