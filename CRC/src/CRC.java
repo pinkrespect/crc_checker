4009507c2ee23a314049217df164e3d0f253dbcf
Maisie <miss.sonagi@gmail.com>
2017-05-29 18:25:51 +0900

    Edit CRC.java

--- CRC/src/CRC.java
+++ CRC/src/CRC.java
@@ -1,12 +1,14 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class CRC {
     static int[] CRC_Checker(int[] input) {
         int[] divisor = new int[]{1, 1, 0, 1, 0, 1};
         int[] result = new int[input.length + divisor.length - 1];
-        List dividend = new ArrayList();
+        ArrayList dividend = new ArrayList();
+
         for(int i = 0; i < result.length; i++){
             if(i < input.length)
                 dividend.add(input[i]);
@@ -14,14 +16,13 @@ public class CRC {
                 dividend.add(0);
         }
 
-        for (int i = 0; i < dividend.size() - divisor.length; i++) {
-            if ((int)dividend.get(i) == 0) {
-                continue;
+        IntStream.range(0, result.length).forEachOrdered(i -> {
+            if((int)dividend.get(i) == 1){
+                IntStream.range(0, divisor.length).forEachOrdered(j -> {
+                    dividend.set(i+j, divisor[j] ^ (int)dividend.get(i+j));
+                });
             }
-            for (int j = 0; j < divisor.length; j++) {
-                dividend.set(i + j, divisor[j] ^ (int)dividend.get(i + j));
-            }
-        }
+        });
 
         for(int i = 0; i < dividend.size(); i++){
             if(i < input.length)
