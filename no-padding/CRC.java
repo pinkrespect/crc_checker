2b3de089cf7ce8dd8ed6494a4d012f0ae8e83ed8
Hyeon Kim <simnalamburt@gmail.com>
2017-06-06 23:16:04 +0900

    Organize codes

--- no-padding/CRC.java
+++ no-padding/CRC.java
@@ -1,37 +1,24 @@
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
-/**
- * Created by maisie on 17. 5. 31.
- */
 public class CRC {
-    static int[] CRC_Checker(int[] input){
-        int[] divisor = new int[]{1, 1, 0, 1, 0, 1};
-        int[] register = new int[divisor.length - 1];
-
-        IntStream.range(0, input.length).forEachOrdered(i -> {
-            int temp = register[0] ^ input[i];
-            IntStream.range(0, register.length).forEachOrdered(j -> {
-                if(j < register.length-1){
-                    if(divisor[j+1] == 1){
-                        register[j] = register[j+1] ^ temp;
-                    }else {
-                        register[j] = register[j+1];
-                    }
-                }else{
-                    register[j] = temp;
-                }
-            });
-            System.out.print("Loop[" + i + "] [ ");
-            for(int k = 0; k < register.length; k++){
-                System.out.print(register[k] + " ");
-            }
-            System.out.print("]\n");
-        });
-        return IntStream.concat(Arrays.stream(input), Arrays.stream(register)).toArray();
-    }
-
     public static void main(String args[]) {
         System.out.println(Arrays.toString(CRC_Checker(new int[]{1, 0, 1, 0, 0, 0, 1, 1, 0, 1})));
+    }
+
+    static int[] CRC_Checker(int[] input) {
+        final int[] divisor = new int[] { 1, 1, 0, 1, 0, 1 };
+        final int[] register = new int[divisor.length - 1];
+
+        IntStream.range(0, input.length).forEachOrdered(i -> {
+            final int temp = register[0] ^ input[i];
+            IntStream.range(0, register.length).forEachOrdered(j -> {
+                register[j] = j >= register.length - 1 ? temp :
+                    divisor[j+1] == 1 ? register[j+1] ^ temp :
+                    register[j+1];
+            });
+        });
+
+        return IntStream.concat(Arrays.stream(input), Arrays.stream(register)).toArray();
     }
 }
