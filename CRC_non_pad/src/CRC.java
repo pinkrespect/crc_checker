baf966d4ee9300025c321615d46b750606fab572
Maisie <miss.sonagi@gmail.com>
2017-06-06 21:56:58 +0900

    Edit indentation CRC.java

--- CRC_non_pad/src/CRC.java
+++ CRC_non_pad/src/CRC.java
@@ -15,23 +15,22 @@ public class CRC {
 				if(j < register.length-1){
 					if(divisor[j+1] == 1){
 						register[j] = register[j+1] ^ temp;
-					}else if(divisor[j+1] == 0){
+					}else {
 						register[j] = register[j+1];
 					}
 				}else{
 					register[j] = temp;
 				}
 			});
-				System.out.print("Loop[" + i + "] [ ");
-				for(int k = 0; k < register.length; k++){
-					System.out.print(register[k] + " ");
-				}
-				System.out.print("]\n");
+			System.out.print("Loop[" + i + "] [ ");
+			for(int k = 0; k < register.length; k++){
+				System.out.print(register[k] + " ");
+			}
+			System.out.print("]\n");
 		});
-
-
 		return IntStream.concat(Arrays.stream(input), Arrays.stream(register)).toArray();
 	}
+
 	public static void main(String args[]) {
 		System.out.println(Arrays.toString(CRC_Checker(new int[]{1, 0, 1, 0, 0, 0, 1, 1, 0, 1})));
 	}
